e2e24d3d08b695db45b3b127cf30a3ab2187a445
abdullahhafiz <abdullahhafiz1997@gmail.com>
2021-02-18 12:34:50 +0500

    vacancy component added

---
+++ src/pages/career/components/Vacancies.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react';
+
+export function Vacancies() {
+    return <Fragment>
+        <section id="services">
+            <div className="container">
+                <div className="section-header">
+                    <h2>Vacancies</h2>
+                    <p> We want to hire React Developers</p>
+                </div>
+            </div>
+        </section>
+        <section id="about" className="wow fadeInUp">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6 content">
+                        <ul>
+                            <h1>Key Requirements</h1>
+                            <li><i className="outline"></i> Experince of 1 to 2 years </li>
+                            <li><i className="outline"></i> Team player</li>
+                            <li><i className="outline"></i> Develop a flexible and well-structured
+                        front-end architecture</li>
+                            <li><i className="outline"></i> Good time-management skills</li>
+                            <li><i className="outline"></i> Responsible for development of new
+                        highly-responsive, web-based user interface</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </Fragment>
+}
