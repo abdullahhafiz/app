c87d5e549569bbf90600bdf33f6c5cafb64a3d99
abdullahhafiz <abdullahhafiz1997@gmail.com>
2021-02-18 12:18:22 +0500

    internship component added

---
+++ src/pages/career/components/internship.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+
+export function Internship(){
+    return <Fragment>
+
+<section id="services">
+        <div className="container">
+            <div className="section-header">
+                <h2>Internship</h2>
+            </div>
+        </div>
+    </section>
+        
+        <section id = 'internship-service'>
+            <div className= 'container'>
+
+            <div className="internship-content">
+                        <ul>
+                            <h1>Key Requirements</h1>
+                            <li><i className='fresh-graduates'></i> Fresh graduates</li>
+                            <li><i className='fresh-graduates-learning'></i> Passionate For Learning</li>
+                            <li><i className='fresh-graduates-complete-task'></i> Complete Tasks On Given Time</li>
+                            <li><i className='fresh-graduates-working-enviroment'></i> Adjust Easily In Working Environment</li>
+                            <li><i className='fresh-graduates-disciplined'></i> Disciplined, Be Punctual In Attendance </li>
+                        </ul>
+                    </div>
+            </div>
+        </section>
+    </Fragment>
+}
