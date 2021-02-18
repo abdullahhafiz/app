c4f76e35070f5e0b93df0c5623faf2cdfa5c858e
abdullahhafiz <abdullahhafiz1997@gmail.com>
2021-02-18 15:22:12 +0500

    path added to appjs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { AboutUs } from './pages/About/AboutUs';
 import { Career } from './pages/career/career';
+import { Contact } from './pages/contact/contact';
 // import { Apply } from './pages/career/components/Apply';
 import { Footer } from './pages/shared/footer';
 import { Header } from './pages/shared/header'
@@ -14,8 +15,9 @@ function App() {
           <Header />
         </div>
         <Switch>
-          <Route exact path = '/about' component={AboutUs}></Route>
-          <Route exact path = '/career' component={Career}></Route>
+          <Route exact path = '/about' component={AboutUs} />
+          <Route exact path = '/career' component={Career} />
+          <Route exact path='/contact' component={Contact} />
 
         </Switch>
         <div>
