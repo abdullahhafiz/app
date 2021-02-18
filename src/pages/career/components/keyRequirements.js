95d54c39ed6624777eed3635a658d20706d6652a
abdullahhafiz <abdullahhafiz1997@gmail.com>
2021-02-18 12:35:51 +0500

    keyrequirement child component added

---
+++ src/pages/career/components/keyRequirements.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+
+export function KeyRequirements(){
+        return <Fragment>
+            <div className='container'>
+            <div className="content">
+                    <ul>
+                        <h1>Key Responsibilities</h1>
+                        <li><i className="icon ion-ios-checkmark-outline"></i> Good english communication skills as well
+                        good team player with a positive and enthusiastic can-do attitude self-starter &
+                        self-learning abilities, and ability to work independently
+                        strong analytical and problem-solving skills
+                    </li>
+                        <li><i className="outline"></i> Be confident and honest with anything you put
+                        up in your resume</li>
+                        <li><i className="outline"></i> Stay true to what you say regarding your
+                        credentials in your interview</li>
+                        <li><i className="outline"></i> Make sure you have some knowledge about your
+                        domain before you apply</li>
+                        <li><i className="outline"></i> Be real and cannot include someone else’s
+                        work in your portfolio. We will appreciate whatever present in your portfolio</li>
+                        <li><i className="outline"></i> Make us believe or satisfy us with your
+                        answers that how your skills can be helpful for our winning team</li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </Fragment>
+
+}
