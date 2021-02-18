78cb0ca50f0f4b2a4a9e8a39fa25458093eb4dba
abdullahhafiz <abdullahhafiz1997@gmail.com>
2021-02-18 12:36:07 +0500

    Requirement child component addded

---
+++ src/pages/career/components/Requirements.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function Requirement() {
+    return <section id="services">
+        <div className="container">
+            <div className="section-header">
+                <h2>Our Requirements</h2>
+                <p>CapregSoft looking for internees to be the part of highly talented and agile software development
+                team. We want to hire fresh graduates for our company</p>
+            </div>
+        </div>
+    </section>
+}
