501d491cdff9d5d24148fd1267b440c6a3cc9018
abdullahhafiz <abdullahhafiz1997@gmail.com>
2021-02-19 12:36:45 +0500

    contact component added

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,26 +1,32 @@
 import React, { Fragment } from 'react';
-import { Address } from './components/Address';
+import { Address } from "./Components/Address";
+import { ContactForm } from "./Components/ContactForm";
+import { Map } from "./Components/Map";
+import { SocialMedia } from "../Shared/SocialMedia";
 
-export function Contact(){
+export function Contact() {
     return <Fragment>
-        <section id = 'innerBanner'>
-            <div className = 'content'>
+        <section id="innerBanner">
+            <div className="inner-content">
                 <h2><span>Contact</span></h2>
+                <div>
+                </div>
             </div>
         </section>
-        <main id = 'main'>
-            <section id ='contact'>
-                <div className='container'>
-                <div className='contact'>
-                    <Address />
-                </div>
+
+        <main id="main">
+            <section id="contact" className="wow fadeInUp">
+                <div className="container">
+                    <div className="section-header">
+                    </div>
+                    <div className="row contact-info">
+                        <Address />
+                        <ContactForm />
+                        <Map />
+                    </div>
                 </div>
             </section>
+            <SocialMedia />
         </main>
-
-
-
-
-
     </Fragment>
 }
