59f36831536228fd5c83af3db2765f6b5f5700c0
abdullahhafiz <abdullahhafiz1997@gmail.com>
2021-02-19 12:34:10 +0500

    vacancies component added

--- src/pages/career/components/Vacancies.js
+++ src/pages/career/components/Vacancies.js
@@ -13,15 +13,18 @@ export function Vacancies() {
         <section id="about" className="wow fadeInUp">
             <div className="container">
                 <div className="row">
+                    <div className="col-lg-6 about-img">
+                        <img src="./assets/img/joboffer.svg" alt="Job" />
+                    </div>
                     <div className="col-lg-6 content">
                         <ul>
                             <h1>Key Requirements</h1>
-                            <li><i className="outline"></i> Experince of 1 to 2 years </li>
-                            <li><i className="outline"></i> Team player</li>
-                            <li><i className="outline"></i> Develop a flexible and well-structured
+                            <li><i className="icon ion-ios-checkmark-outline"></i> Experince of 1 to 2 years </li>
+                            <li><i className="icon ion-ios-checkmark-outline"></i> Team player</li>
+                            <li><i className="icon ion-ios-checkmark-outline"></i> Develop a flexible and well-structured
                         front-end architecture</li>
-                            <li><i className="outline"></i> Good time-management skills</li>
-                            <li><i className="outline"></i> Responsible for development of new
+                            <li><i className="icon ion-ios-checkmark-outline"></i> Good time-management skills</li>
+                            <li><i className="icon ion-ios-checkmark-outline"></i> Responsible for development of new
                         highly-responsive, web-based user interface</li>
                         </ul>
                     </div>
