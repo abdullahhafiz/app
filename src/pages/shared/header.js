46941e6001bec4729924618ad2116ab3acc4c66e
abdullahhafiz <abdullahhafiz1997@gmail.com>
2021-02-19 12:23:51 +0500

    header js page updated

--- src/pages/shared/header.js
+++ src/pages/shared/header.js
@@ -4,11 +4,22 @@ import { Link } from 'react-router-dom';
 export function Header() {
     return <header id="header">
         <div className="container">
+            <div id="logo" className="pull-left">
+                <Link to="/" id="body" className="scrollto">
+                    <span>
+                        <div className="logo">
+                            <img src="./assets/img/WebLogo.png" alt="logo" style={{ height: "75px" }} />
+                        </div>
+                    </span>
+                </Link>
+            </div>
             <nav id="nav-menu-container">
                 <ul className="nav-menu">
+                    <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About Us</Link></li>
+                    <li><Link to="/services">Services</Link></li>
+                    <li><Link to="/Contact">Contact</Link></li>
                     <li><Link to="/Career">Career</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </div>
